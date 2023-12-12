b37c56e0de8e7e5714143493d7177f6058290831
SimbanaPablo <pablo.simbana01@gmail.com>
2023-12-11 19:45:27 -0500

    taller14

---
+++ src/main/java/com/uce/edu/ec/Pa2U2P5PsApplication.java
@@ -0,0 +1,13 @@
+package com.uce.edu.ec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pa2U2P5PsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pa2U2P5PsApplication.class, args);
+	}
+
+}
