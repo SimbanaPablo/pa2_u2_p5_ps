d794befc0e1cc0e96222ed3bb12bd9708e46e427
SimbanaPablo <pablo.simbana01@gmail.com>
2023-12-12 19:48:50 -0500

    taller15

---
+++ src/main/java/com/uce/edu/ec/repository/modelo/Alumno.java
@@ -0,0 +1,37 @@
+package com.uce.edu.ec.repository.modelo;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "alumno")
+public class Alumno {
+	@Id
+	@Column(name = "alum_id")
+	@SequenceGenerator(name = "generador",sequenceName = "seq_alumno",allocationSize = 1)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_alumno")
+	private Integer id;
+	@Column(name = "alum_nombre")
+	private String nombre;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+}
---
+++ src/main/java/com/uce/edu/ec/repository/modelo/Estudiante.java
@@ -0,0 +1,73 @@
+package com.uce.edu.ec.repository.modelo;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "estudiante")
+public class Estudiante {
+	@Column(name = "estu_id")
+	@Id
+	@SequenceGenerator(name = "generador",sequenceName = "seq_estudiante",allocationSize = 1)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "generador")
+	private Integer id;
+
+	@Column(name = "estu_nombre")
+	private String nombre;
+
+	@Column(name = "estu_apellido")
+	private String apellido;
+
+	@Column(name = "estu_cedula")
+	private String cedula;
+
+	@Column(name = "estu_fecha_nacimiento")
+	private LocalDateTime fechaNacimiento;
+
+	public LocalDateTime getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+
+	public void setFechaNacimiento(LocalDateTime fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getCedula() {
+		return cedula;
+	}
+
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+}
